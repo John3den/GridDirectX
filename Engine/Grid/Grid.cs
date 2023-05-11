3d1456f93e2ba23de0f6f04fa43d26c5fadc58bf
John3den <64809077+John3den@users.noreply.github.com>
2023-05-11 17:46:39 +0700

    Grid reading refactor

--- Engine/Grid/Grid.cs
+++ Engine/Grid/Grid.cs
@@ -1,3 +1,4 @@
+using DevExpress.Data;
 using DevExpress.XtraEditors;
 using DevExpress.XtraPrinting.BarCode;
 using GridRender;
@@ -33,6 +34,12 @@ namespace Engine
         string _pathToCells;
         Device _device;
 
+        // top -> bottom -> front -> back -> left -> right
+        int[] vertexOrder = { 0, 1, 3, 2, 3, 1,  4, 7, 5, 6, 5, 7,
+                              1, 0, 5, 4, 5, 0,  3, 2, 6, 7, 3, 6,
+                              0, 3, 7, 7, 4, 0,  2, 1, 6, 5, 6, 1 };
+        // corner 0 -> corner 1 -> corner 2 -> corner 3
+        int[] vertexReadOrder = { 0, 4, 1, 5, 2, 6, 3, 7 };
         public void GenerateCells()
         {
             GridReader reader = new GridReader(_pathToCells);
@@ -45,58 +52,19 @@ namespace Engine
                     for (int j = 0; j < _size.y; j++)
                     {
                         bool act = reader.GetCellStatus();
-                        Vector3 v0 = reader.GetCellVertex();
-                        Vector3 v4 = reader.GetCellVertex();
-                        Vector3 v1 = reader.GetCellVertex();
-                        Vector3 v5 = reader.GetCellVertex();
-                        Vector3 v2 = reader.GetCellVertex();
-                        Vector3 v6 = reader.GetCellVertex();
-                        Vector3 v3 = reader.GetCellVertex();
-                        Vector3 v7 = reader.GetCellVertex();
+                        Vector3[] currentCellVertices = new Vector3[8];
+
+                        for (int h = 0; h < 8; h++) 
+                        {
+                            currentCellVertices[vertexReadOrder[h]] = reader.GetCellVertex();
+                        }
+
                         Vector3[] posData = new Vector3[36];
 
-                        //TOP
-                        posData[0] = v0;
-                        posData[1] = v1;
-                        posData[2] = v3;
-                        posData[3] = v2;
-                        posData[4] = v3;
-                        posData[5] = v1;
-                        //BOTTOM
-                        posData[6] = v4;
-                        posData[7] = v7;
-                        posData[8] = v5;
-                        posData[9] = v6;
-                        posData[10] = v5;
-                        posData[11] = v7;
-                        //FRONT
-                        posData[12] = v1;
-                        posData[13] = v0;
-                        posData[14] = v5;
-                        posData[15] = v4;
-                        posData[16] = v5;
-                        posData[17] = v0;
-                        //BACK
-                        posData[18] = v3;
-                        posData[19] = v2;
-                        posData[20] = v6;
-                        posData[21] = v7;
-                        posData[22] = v3;
-                        posData[23] = v6;
-                        //LEFT
-                        posData[24] = v0;
-                        posData[25] = v3;
-                        posData[26] = v7;
-                        posData[27] = v7;
-                        posData[28] = v4;
-                        posData[29] = v0;
-                        //RIGHT
-                        posData[30] = v2;
-                        posData[31] = v1;
-                        posData[32] = v6;
-                        posData[33] = v5;
-                        posData[34] = v6;
-                        posData[35] = v1;
+                        for (int h = 0; h < 36; h++) 
+                        {
+                            posData[h] = currentCellVertices[vertexOrder[h]];
+                        }
 
                         float normalizedCellYPosition = (float)k / (float)_size.z;
                         Cell cell = new Cell(posData, act, _device, normalizedCellYPosition, Properties[_currentProperty].Values[i, j, k], Properties[_currentProperty].GetOffset(), Properties[_currentProperty].GetScaling());
